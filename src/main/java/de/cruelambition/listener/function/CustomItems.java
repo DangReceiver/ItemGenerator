ed72de967c620648d99290823205ee47efe56fd4
DangReceiver <TearsDontFall.TDF@gmail.com>
2023-10-24 08:50:04 +0200

    jetpack tweaks, Debugging custom items

--- src/main/java/de/cruelambition/listener/function/CustomItems.java
+++ src/main/java/de/cruelambition/listener/function/CustomItems.java
@@ -21,111 +21,112 @@ import java.util.Random;
 
 public class CustomItems implements Listener {
 
-	@EventHandler
-	public void handle(PlayerInteractEvent e) {
-		Player p = e.getPlayer();
-		ItemStack item = e.getItem();
-		if (item == null) return;
+    @EventHandler
+    public void handle(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        ItemStack item = e.getItem();
+        if (item == null) return;
 
-		Action a = e.getAction();
-		Block cb = e.getClickedBlock();
+        Action a = e.getAction();
+        Block cb = e.getClickedBlock();
 
-		Lang l = new Lang(p);
+        Lang l = new Lang(p);
 
-		if (item.equals(Items.ITEMS.get(3))) {
-			e.setCancelled(true);
-			p.sendMessage("Crate");
+        if (item.equals(Items.ITEMS.get(3))) {
+            e.setCancelled(true);
+            p.sendMessage("Crate");
 
-			if (a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK) {
-				p.sendMessage("some action idk yet");
-			}
+            if (a == Action.RIGHT_CLICK_AIR || a == Action.RIGHT_CLICK_BLOCK) {
+                p.sendMessage("some action idk yet");
+            }
 
-		} else if (item.equals(Items.ITEMS.get(2))) {
-			e.setCancelled(true);
-			p.sendMessage("eraser");
+        } else if (item.equals(Items.ITEMS.get(2))) {
+            e.setCancelled(true);
+            p.sendMessage("eraser");
 
-			if (a == Action.LEFT_CLICK_BLOCK && cb != null) {
+            if (a == Action.LEFT_CLICK_BLOCK && cb != null) {
 
-				int i = 0;
-				checkNextBlocks(cb, i);
+                int i = 0;
+                // needs to be reworked
+                checkNextBlocks(cb, i);
 
-			} else {
-				p.sendMessage("need to left click a block");
-			}
+            } else {
+                p.sendMessage("need to left click a block");
+            }
 
-		} else if (item.equals(Items.ITEMS.get(1))) {
-			e.setCancelled(true);
-			p.sendMessage("sound");
+        } else if (item.equals(Items.ITEMS.get(1))) {
+            e.setCancelled(true);
+            p.sendMessage("sound");
 
-			if (a.toString().contains("RIGHT") || a.toString().contains("LEFT")) {
-				Random r = new Random();
+            if (a.toString().contains("RIGHT") || a.toString().contains("LEFT")) {
+                Random r = new Random();
 
-				p.playSound(p.getLocation(), Sound.values()[r.nextInt(Sound.values().length - 1)],
-						0.75f, (float) (r.nextInt(21) / 10));
-			}
+                p.playSound(p.getLocation(), Sound.values()[r.nextInt(Sound.values().length - 1)],
+                        0.8f, (float) (r.nextInt(21) / 10));
+            }
 
-		} else if (item.equals(Items.ITEMS.get(0))) {
-			e.setCancelled(true);
+        } else if (item.equals(Items.ITEMS.get(0))) {
+            e.setCancelled(true);
 
-			PC pc = new PC(p);
-			if (a.toString().contains("RIGHT")) {
+            PC pc = new PC(p);
+            if (a.toString().contains("RIGHT")) {
 
-				if (pc.getJetpackUsage()) {
-					p.sendTitle(Lang.PRE, "§7" + l.getString("customitems_listener_jetpack_in_use"));
-					return;
-				}
+                if (pc.getJetpackUsage()) {
+                    p.sendTitle(Lang.PRE, "§7" + l.getString("customitems_listener_jetpack_in_use"));
+                    return;
+                }
 
-				pc.setJetpackUsage(true);
-				p.setVelocity(p.getVelocity().add(new Vector(0, 0.25, 0)));
+                pc.setJetpackUsage(true);
+                p.setVelocity(p.getVelocity().add(new Vector(0, 0.125, 0)).multiply(1.12f));
 
-				Bukkit.getScheduler().runTaskLater(ItemGenerator.getItemGenerator(), () -> {
-					p.setVelocity(p.getVelocity().add(new Vector(0, 0.35, 0)));
-					pc.setJetpackUsage(false);
-				}, 4);
-			}
-		}
-	}
+                Bukkit.getScheduler().runTaskLater(ItemGenerator.getItemGenerator(), () -> {
+                    p.setVelocity(p.getVelocity().add(new Vector(0, 0.1f, 0)).multiply(1.2f));
+                    pc.setJetpackUsage(false);
+                }, 4);
+            }
+        }
+    }
 
-	public void checkNextBlocks(Block cb, int i) {
-		Material type = cb.getType();
-		cb.getWorld().dropItemNaturally(cb.getLocation().add(0, 0.5, 0), new ItemStack(type));
+    public void checkNextBlocks(Block cb, int i) {
+        Material type = cb.getType();
+        cb.getWorld().dropItemNaturally(cb.getLocation().add(0, 0.5, 0), new ItemStack(type));
 
-		if (i >= 4) return;
+        if (i >= 4) return;
 
-		Block xm = cb.getLocation().add(-1, 0, 0).getBlock();
-		if (type == xm.getType()) {
-			checkNextBlocks(xm, i + 1);
-			xm.setType(Material.AIR);
-		}
+        Block xm = cb.getLocation().add(-1, 0, 0).getBlock();
+        if (type == xm.getType()) {
+            checkNextBlocks(xm, i + 1);
+            xm.setType(Material.AIR);
+        }
 
-		Block xp = cb.getLocation().add(1, 0, 0).getBlock();
-		if (type == xp.getType()) {
-			checkNextBlocks(xp, i + 1);
-			xp.setType(Material.AIR);
-		}
+        Block xp = cb.getLocation().add(1, 0, 0).getBlock();
+        if (type == xp.getType()) {
+            checkNextBlocks(xp, i + 1);
+            xp.setType(Material.AIR);
+        }
 
-		Block ym = cb.getLocation().add(0, -1, 0).getBlock();
-		if (type == ym.getType()) {
-			checkNextBlocks(ym, i + 1);
-			ym.setType(Material.AIR);
-		}
+        Block ym = cb.getLocation().add(0, -1, 0).getBlock();
+        if (type == ym.getType()) {
+            checkNextBlocks(ym, i + 1);
+            ym.setType(Material.AIR);
+        }
 
-		Block yp = cb.getLocation().add(0, 1, 0).getBlock();
-		if (type == yp.getType()) {
-			checkNextBlocks(yp, i + 1);
-			yp.setType(Material.AIR);
-		}
+        Block yp = cb.getLocation().add(0, 1, 0).getBlock();
+        if (type == yp.getType()) {
+            checkNextBlocks(yp, i + 1);
+            yp.setType(Material.AIR);
+        }
 
-		Block zm = cb.getLocation().add(0, 0, -1).getBlock();
-		if (type == zm.getType()) {
-			checkNextBlocks(zm, i + 1);
-			zm.setType(Material.AIR);
-		}
+        Block zm = cb.getLocation().add(0, 0, -1).getBlock();
+        if (type == zm.getType()) {
+            checkNextBlocks(zm, i + 1);
+            zm.setType(Material.AIR);
+        }
 
-		Block zp = cb.getLocation().add(0, 0, 1).getBlock();
-		if (type == zp.getType()) {
-			checkNextBlocks(zp, i + 1);
-			zp.setType(Material.AIR);
-		}
-	}
+        Block zp = cb.getLocation().add(0, 0, 1).getBlock();
+        if (type == zp.getType()) {
+            checkNextBlocks(zp, i + 1);
+            zp.setType(Material.AIR);
+        }
+    }
 }
--- src/main/java/de/cruelambition/listener/function/Furnace.java
+++ src/main/java/de/cruelambition/listener/function/Furnace.java
@@ -12,6 +12,7 @@ public class Furnace implements Listener {
     @EventHandler
     public void handle(FurnaceStartSmeltEvent e) {
         for (Player ap : Bukkit.getOnlinePlayers()) {
+            ap.sendMessage("0");
 
             ap.sendMessage("Recipe: " + e.getRecipe());
             ap.sendMessage("Total Cook Time: " + e.getTotalCookTime());
@@ -25,6 +26,7 @@ public class Furnace implements Listener {
     @EventHandler
     public void handle(FurnaceSmeltEvent e) {
         for (Player ap : Bukkit.getOnlinePlayers()) {
+            ap.sendMessage("1");
 
             ap.sendMessage("Recipe: " + e.getRecipe());
             ap.sendMessage("Result: " + e.getResult());
@@ -37,6 +39,7 @@ public class Furnace implements Listener {
     @EventHandler
     public void handle(FurnaceBurnEvent e) {
         for (Player ap : Bukkit.getOnlinePlayers()) {
+            ap.sendMessage("2");
 
             ap.sendMessage("Block (type): " + e.getBlock().getType());
             ap.sendMessage("Burn Time: " + e.getBurnTime());
@@ -47,6 +50,7 @@ public class Furnace implements Listener {
     @EventHandler
     public void handle(FurnaceExtractEvent e) {
         for (Player ap : Bukkit.getOnlinePlayers()) {
+            ap.sendMessage("3");
 
             ap.sendMessage("Player: " + e.getPlayer().getName());
             ap.sendMessage("Block (type): " + e.getBlock().getType());
@@ -61,6 +65,8 @@ public class Furnace implements Listener {
     @EventHandler
     public void handle(BrewingStartEvent e) {
         for (Player ap : Bukkit.getOnlinePlayers()) {
+            ap.sendMessage("4");
+
             ap.sendMessage("Block (type): " + e.getBlock().getType());
             ap.sendMessage("Source: " +e.getSource());
             ap.sendMessage("Brew Time: " +e.getTotalBrewTime());
@@ -70,6 +76,8 @@ public class Furnace implements Listener {
     @EventHandler
     public void handle(BrewingStandFuelEvent e) {
         for (Player ap : Bukkit.getOnlinePlayers()) {
+            ap.sendMessage("5");
+
             ap.sendMessage("Fuel: " + e.getFuel());
             ap.sendMessage("Block (type): " + e.getBlock().getType());
             ap.sendMessage("Fuel Power: " + e.getFuelPower());
